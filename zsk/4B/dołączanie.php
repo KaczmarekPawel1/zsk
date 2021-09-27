95405e36be18ca972405b4be2d31018b9c55aa11
KaczmarekPawel1 <89963404+KaczmarekPawel1@users.noreply.github.com>
2021-09-27 22:17:59 +0200

    Add files via upload

---
+++ zsk/4B/dołączanie.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>Początek strony</h3>
+    <?php
+      include './plik.php';
+     ?>
+    <h3>Koniec storny</h3>
+  </body>
+</html>
