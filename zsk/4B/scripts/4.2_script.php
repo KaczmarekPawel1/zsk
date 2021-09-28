23b4c7b1717c86b7c75bf20bbc79bb6555a998ea
KaczmarekPawel1 <89963404+KaczmarekPawel1@users.noreply.github.com>
2021-09-28 14:54:31 +0200

    Add files via upload

---
+++ zsk/4B/scripts/4.2_script.php
@@ -0,0 +1,26 @@
+<?php
+echo ("<pre>");
+print_r ($_POST);
+echo ("</pre>");
+if (!empty ($_POST[]'name']) && !empty($_POST['geometricFigure'])){
+  echo 'ok';
+
+}else{
+  switch (variable) {
+    case 'kwadrat':
+      header("location ./square.php");
+      break;
+
+    case 'prostokat':
+
+      break;
+  } 'Wypełnij dane';
+}
+ ?>
+   <script>
+    history.back();
+   <script>
+   <?php
+ }
+
+    ?>
