6bbfe94933e3442cfbe9a9f1f3097aad17546fde
KaczmarekPawel1 <89963404+KaczmarekPawel1@users.noreply.github.com>
2021-09-29 19:14:55 +0200

    Update 4.2_.php

--- zsk/4B/4.2_.php
+++ zsk/4B/4.2_.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="pl" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <title>Figury Geometryczne</title>
-</head>
-<body>
-  <h3>Figury geometryczne</h3>
-  <form action="./scrpits/4.2_script.php" method="post">
-    <imput type="text" name="name" placeholder="Podaj imię"><br><br>
-    <input type="radio" name="geometricFigure" value="kwadrat" checked>Kwadrat<br>
-    <input type="radio" name="geometricFigure" value="Prostokat">Prostokąt<br><br>
-    <input type="submit" value="Zatwierdz figure">
-  </form>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <title>Figury geometryczne</title>
+  </head>
+  <body>
+    <h3>Figury geometryczne</h3>
+    <form action ="./scripts/4_2_script.php" method="post">
+      <input type="text" name="name" placeholder="Podaj imie"><br><br>
+<input type="radio" name="geometricFigure" value="kwadrat" checked>Kwadrat<br>
+<input type="radio" name="geometricFigure" value="prostokąt">prostokąt<br><br>
+<input type="submit" value="Zatwierdz figure">
+    </form>
+  </body>
 </html>
